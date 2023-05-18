5455ac5d22f50051c92ceeaa27c1cbceaefaeda0
Joey Vitale <joey.vitale31@gmail.com>
2023-05-18 13:00:48 +0100

    styling added

---
+++ bs.js
@@ -0,0 +1,24 @@
+const App = () => {
+    const handleButtonClick = () => {
+      const name = document.getElementById("nameInput").value;
+      alert(`Hello, ${name}!`);
+    };
+  
+    return (
+      <div className="container">
+        <img src="company-logo.png" alt="Company Logo" className="logo" />
+        <input
+          type="text"
+          id="nameInput"
+          placeholder="Enter your name"
+          className="textbox"
+        />
+        <button className="button" onClick={handleButtonClick}>
+          Submit
+        </button>
+      </div>
+    );
+  };
+  
+  ReactDOM.render(<App />, document.getElementById("root"));
+  
