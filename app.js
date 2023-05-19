ce6630a2e0824dc72e7fb700b03eb8db87e206fd
Joey Vitale <joey.vitale31@gmail.com>
2023-05-19 13:44:23 +0100

    updated/cleaning/added pyserver/flask

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to Company Website</h1>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+      <p>Company Name</p>
+      <input type="text" placeholder="Enter your name" className="textbox" />
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h1>Contact</h1>
+      <p>Contact information goes here</p>
+    </div>
+  );
+};
+
 const App = () => {
-    const handleButtonClick = () => {
-      const name = document.getElementById("nameInput").value;
-      alert(`Hello, ${name}!`);
-    };
-  
-    return (
-      <div className="container">
-        <img src="logotemp.jpg" alt="Company Logo" className="logo" />
-        <input
-          type="text"
-          id="nameInput"
-          placeholder="Enter your name"
-          className="textbox"
-        />
-        <button className="button" onClick={handleButtonClick}>
-          Submit
-        </button>
+  return (
+    <Router>
+      <nav className="navbar">
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/about" className="nav-link">About</Link>
+        <Link to="/contact" className="nav-link">Contact</Link>
+      </nav>
+      <div className="page-content">
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </div>
-    );
-  };
-  
-  ReactDOM.render(<App />, document.getElementById("root"));
-  
+    </Router>
+  );
+};
+
+export default App;
